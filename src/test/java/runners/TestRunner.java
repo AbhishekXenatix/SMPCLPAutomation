3f34194acf8e7d5f0fa320a238c90c5e01dfecc1
Abhishek_Gupta3 <abhishekgupta183@gmail.com>
2023-05-21 16:30:28 +0800

    initial commit 10

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,29 +1,19 @@
 package runners;
 
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 
-
-
-@Test
-
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepdefinitions"},
         plugin = {"pretty",
-                "html:target/cucumber-reports/cucumber-reports.html",
-                "json:target/cucumber-reports/cucumber-reports.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
-        , monochrome = true
-)
-public class TestRunner extends AbstractTestNGCucumberTests {
+                "json:target/cucumber-reports/cucumber.json"
+               },
+        monochrome = true)
+public class TestRunner{
 
-    @Override
-    @DataProvider(parallel = false)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
+
 }
